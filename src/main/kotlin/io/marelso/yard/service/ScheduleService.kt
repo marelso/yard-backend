280c6ce9bb908ac6a22cc0c624d113dd90e3082e
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 05:21:16 -0300

    Including new service to handle schedule requests

---
+++ src/main/kotlin/io/marelso/yard/service/ScheduleService.kt
@@ -0,0 +1,9 @@
+package io.marelso.yard.service
+
+import io.marelso.yard.repository.ScheduleRepository
+import org.springframework.stereotype.Service
+
+@Service
+class ScheduleService(private val repository: ScheduleRepository) {
+
+}
