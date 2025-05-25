21de9833cfce0ed1051288e095c12533731b71da
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 05:06:47 -0300

    Including missing fields

--- src/main/kotlin/io/marelso/yard/domain/Schedule.kt
+++ src/main/kotlin/io/marelso/yard/domain/Schedule.kt
@@ -1,6 +1,10 @@
 package io.marelso.yard.domain
 
+import java.time.OffsetDateTime
+
 data class Schedule(
     val id: String? = null,
     val type: String? = null,
+    val amount: Int? = null,
+    val date: OffsetDateTime? = null,
 )
