55204ea55ee8ccd8ed991f1933dd66c8d08cc43f
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 05:07:03 -0300

    Including data dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
