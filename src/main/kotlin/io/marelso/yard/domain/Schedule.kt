09f7ecc6efc2157f68bf627e3ce868957920c19c
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 05:46:19 -0300

    Updating schedule date property data model

--- src/main/kotlin/io/marelso/yard/domain/Schedule.kt
+++ src/main/kotlin/io/marelso/yard/domain/Schedule.kt
@@ -1,10 +1,8 @@
 package io.marelso.yard.domain
 
-import java.time.OffsetDateTime
-
 data class Schedule(
     val id: String? = null,
     val type: ScheduleType? = null,
     val amount: Int? = null,
-    val date: OffsetDateTime? = null,
+    val date: String? = null,
 )
