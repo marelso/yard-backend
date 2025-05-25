5de4c85ac38e2ead2e761a6c0b8b9abf3bd0421b
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 04:17:52 -0300

    Initial create

---
+++ src/test/kotlin/io/yard/Yard/YardApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.yard.Yard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
