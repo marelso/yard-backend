068e62a3abc20bd84cb3b0a30744e12795eba4f5
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 05:46:34 -0300

    Including base methods for create and delete schedule

--- src/main/kotlin/io/marelso/yard/controller/ScheduleController.kt
+++ src/main/kotlin/io/marelso/yard/controller/ScheduleController.kt
@@ -1,10 +1,18 @@
 package io.marelso.yard.controller
 
+import io.marelso.yard.domain.dto.CreateScheduleDTO
 import io.marelso.yard.service.ScheduleService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/schedule")
 class ScheduleController(private val scheduleService: ScheduleService) {
+    @PostMapping
+    fun post(@RequestBody dto: CreateScheduleDTO) = ResponseEntity.ok(scheduleService.create(dto))
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable("id") id: String) = scheduleService.delete(id)
 }
--- src/main/kotlin/io/marelso/yard/service/ScheduleService.kt
+++ src/main/kotlin/io/marelso/yard/service/ScheduleService.kt
@@ -1,9 +1,17 @@
 package io.marelso.yard.service
 
+import io.marelso.yard.domain.Schedule
+import io.marelso.yard.domain.dto.CreateScheduleDTO
+import io.marelso.yard.domain.factory.ScheduleFactory
 import io.marelso.yard.repository.ScheduleRepository
 import org.springframework.stereotype.Service
 
 @Service
-class ScheduleService(private val repository: ScheduleRepository) {
+class ScheduleService(
+    private val repository: ScheduleRepository,
+    private val factory: ScheduleFactory
+) {
+    fun create(dto: CreateScheduleDTO): Schedule = repository.save(factory.from(dto))
 
+    fun delete(id: String) = repository.deleteById(id)
 }
