c1c3cfd05519d8751355626611e4304f4c2177fd
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 04:55:26 -0300

    Updating packages

---
+++ src/test/kotlin/io/marelso/yard/YardApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.marelso.yard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
