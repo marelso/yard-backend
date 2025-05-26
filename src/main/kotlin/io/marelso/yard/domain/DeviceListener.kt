9db4f3d0b0d078724ac4e40cf974f842cc716834
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-25 22:18:50 -0300

    Include new data model to handle listeners

---
+++ src/main/kotlin/io/marelso/yard/domain/DeviceListener.kt
@@ -0,0 +1,8 @@
+package io.marelso.yard.domain
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+
+data class DeviceListener(
+    val reference: String,
+    val emitter: SseEmitter
+)
